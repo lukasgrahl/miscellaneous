d55fbd1d768ea7a25e7eebb4bd15a98150c3300e
Lukas Grahl <lukas.g@amkat.de>
2023-09-03 12:47:46 +0200

    added transitivity example

--- src/transitivity.py
+++ src/transitivity.py
@@ -2,8 +2,47 @@ import numpy as np
 import pandas as pd
 from itertools import chain, combinations
 
-def get_candidates(all_tokens, similarity_factor: float = .8):
 
+def is_matrix_transitive(M) -> bool:
+    """
+    This function checks matrix transitivity checking whether zeros "survive" matrix multiplication
+    :param M: matrix of interest
+    :return: is transitive
+    """
+    return np.sum(np.sum((((M @ M) > 0) * 1) - M, axis=0), axis=0) == 0
+
+
+def to_unity(v) -> np.array:
+    """
+    this function norms a vector to unity as implemented in spacy
+    :param v: vector
+    :return: normed vector
+    """
+    norm = np.linalg.norm(v)
+    if norm == 0:
+        return v
+    else:
+        return v / norm
+
+
+def vec_similarity(a: np.array, b: np.array) -> np.ndarray:
+    """
+    The similarity score of two vectors is calculated as the dot product of two vectors normed to unity
+    :param a:
+    :param b:
+    :return:
+    """
+    return np.dot(to_unity(a), to_unity(b))
+
+
+def get_transitivity_candidates(all_tokens, similarity_threshold: float = .8) -> np.array:
+    """
+    This function considers all token pairs and returns an array of pairs with a similarity score larger than
+    the similarity threshold
+    :param all_tokens: list of spacy tokens
+    :param similarity_threshold:
+    :return: array of format np.array([["lemma1", "lemma2", bool]])
+    """
     lemmas = [i.lemma_ for i in all_tokens]
     unq_str_tokens_idx = [lemmas.index(i) for i in set(lemmas)]
     unq_str_tokens = [[all_tokens[i]] for i in unq_str_tokens_idx]
@@ -13,36 +52,12 @@ def get_candidates(all_tokens, similarity_factor: float = .8):
 
     similarity = [vec_similarity(i[0].vector, i[1].vector) for i in combs]
     df = pd.DataFrame(similarity, index=pd.Index([(i[0].lemma_, i[1].lemma_) for i in combs]), columns=['similarity'])
-    df['bool_sim'] = df.similarity >= similarity_factor
-    df = df[df.bool_sim].reset_index()
-    df = df.drop('similarity', axis=1)
+    similar_pairs = df[df.similarity >= similarity_threshold].reset_index().values[:, :2]
 
-    return df.values
+    return similar_pairs
 
 
-def is_matrix_transitive(M):
-    return np.sum((((M @ M) > 0) * 1) - M) == 0
-
-def to_unity(v):
-    norm = np.linalg.norm(v)
-    if norm == 0:
-        return v
-    else:
-        return v / norm
-
-def vec_similarity(a, b) -> np.ndarray:
-    return np.dot(to_unity(a), to_unity(b))
-
-def get_vector_similarity(a, b) -> np.ndarray:
-    """
-    returns the similarity of two NEntityClusters
-    :param a:
-    :param b:
-    :return:
-    """
-    return vec_similarity(a.vector, b.vector)
-
-def _transitvity_check_first_level (all_pairs: np.array, pairs_tb_checked: list) -> set:
+def transitivity_check_first_level(all_pairs: np.array, pairs_tb_checked: list) -> set:
     """
     1st step for checking transitvity
     For a given set of pairs with relation A (pairs_tb_checked), this functions finds all other pairs in (all_pairs)
@@ -59,10 +74,11 @@ def _transitvity_check_first_level (all_pairs: np.array, pairs_tb_checked: list)
         for str_tb_checked in pairs_tb_checked:
             # XOR should be excluded through drop duplicates, thus OR
             if (str_tb_checked == item[0]) ^ (str_tb_checked == item[1]):
-                set_topics.add(tuple (item [:2]))
+                set_topics.add(tuple(item[:2]))
     return set_topics
 
-def _transitivity_check_second_level(set_topics2: set, set_topics1: set) -> list:
+
+def transitivity_check_second_level(set_topics2: set, set_topics1: set) -> list:
     """
     2nd level to transitivity check
     For each word in the past iterations set of pairs (set_topics1), check wether from step 1 new pairs have been added,
@@ -84,65 +100,76 @@ def _transitivity_check_second_level(set_topics2: set, set_topics1: set) -> list
             )
         ]
     ]
-    return list(set (new_items))
+    return list(set(new_items))
 
-def _recursion_transitive_clusters(candidates: np.array, max_recursion: int = 40) -> list:
+
+def get_transitive_cluster(arr_similar_pairs: np.array, max_recursion: int = 100) -> list:
     """
-    This function recursively builds a set of pairs that are linked from a list of candidates.
-     For each such set of linked pairs, this function then checks transitivty
-    :param candidates: np.array of tuple pairs
-    :param max_recursion:
-    :return:
+    This functions obtains cluster candidates by checking which similar pairs are connected
+    The cluster candidate is obtained once all interconnections are found, it is then checked for transitivity
+    :param arr_similar_pairs: np.array of expression pair tuples
+    :param max_recursion: maximum number of recursions, to control runtime
+    :return: list of clusters and boolean for transitivity
     """
-    out = [] # output
-    lst_has_been_checked = [] # list containing all expression that have been checked
+    OUTPUT = []  # output of cluster with boolean for transitivity
+    LST_HAS_BEEN_CHECKED = []  # list of all expression which have been checked as part of a cluster candidate
 
-    # print(f'started recursion for {candidates.shape[0]}') # candidates: contains pairs of candidates which stand in relation A
-    for idx in range(0, candidates.shape[0]): # progressBar([*range (0, candidates.shape[0])], **kwargs):
+    for idx in range(0, arr_similar_pairs.shape[0]):
 
-        counter = 0 # recursion counter
-        #word #1 A word#2 -> 1st_tb_checked for other pairs containig either #1 xor #2
+        counter = 0  # recursion counter
+        # expressions which have not been checked for interconections
+        lst_tb_checked_recursion = list(arr_similar_pairs[idx])
 
-        lst_tb_checked = list(candidates[idx, :2])
-
-        # if canidates have been checked already skip
-        # candidates might be related to a previously explored set, thus avoid unecessary runs
-
-        if (lst_tb_checked[0] in lst_has_been_checked) and (lst_tb_checked [1] in lst_has_been_checked):
+        # if both expression are contained in a previous cluster candidate skip
+        if (lst_tb_checked_recursion[0] in LST_HAS_BEEN_CHECKED) \
+                and (lst_tb_checked_recursion[1] in LST_HAS_BEEN_CHECKED):
             continue
-        # set of all pairs with relation A, which are linked to either #1 or #2
 
-        OVERALL_SET = set()
-        OVERALL_SET.add(tuple (lst_tb_checked))
+        # cluster candidate consisting of one or more pairs
+        set_cluster_candidates = set()
+        set_cluster_candidates.add(tuple(lst_tb_checked_recursion))
 
+        while len(lst_tb_checked_recursion) > 0:
+            # 1st level: finds all pairs that contain the expression from lst_tb_checked_recursion
+            set_topics = transitivity_check_first_level(arr_similar_pairs, lst_tb_checked_recursion)
+            # 2nd level: checks whether the new pairs from set_topics, link to other pairs -> recursion
+            lst_tb_checked_recursion = transitivity_check_second_level(set_topics, set_cluster_candidates)
+            # updating the cluster candidates
+            set_cluster_candidates.update(set_topics)
 
-        while len(lst_tb_checked) > 0:
-            # 1st level: finds all pairs that contain (xor) words in lst_tb_checked set_topics = _transitvity_check_first_level(candidates, lst_tb_checked) # 2nd level: checks whether adding new pairs has added new words #N 1st_tb_checked = _transitivity_check_second_level(set_topics, OVERALL_SET) OVERALL SET.update(set_topics)
-            # for new words #N, pairs containing #N words are added
-            # adding new pairs might add new words # the recursion stops when no new words, requiring adding new pairs occur
-            # recursion max to avoid infinite loop, adjust as needed
             if counter > max_recursion:
-                # logger.warning(f'maximal recursion depth (max_recursion} has been reached for: {candidates}')
-                print(f'maximal recursion depth {max_recursion} has been reached for: {candidates}')
+                print(f'maximal recursion depth {max_recursion} has been reached for: {arr_similar_pairs}')
                 break
-
             # tracking recursion depth
             counter += 1
 
-            # each pair yields a set of related pairs, these can be represented in a matrix
-            matrix = pd.DataFrame(*chain(*[[(i[0], i[1], True), (i[1], i[0], True)]for i in OVERALL_SET]),
-                                  columns = ['Level1', 'level2', 'is_relation'])
-            matrix = matrix.pivot_table(index='level1', columns='level2', values='is_relation').fillna(0)
-            np.fill_diagonal(matrix.values, True)  # fill diagonal to be True, (true by definition as word#1 == word#1)
+        # each cluster candiates can be represented in a matrix, where the relation between expressions is a boolean
+        matrix = pd.DataFrame(
+            [
+                *chain(
+                    *[
+                        [
+                            (i[0], i[1], True), (i[1], i[0], True)
+                        ] for i in set_cluster_candidates
+                    ]
+                )
+            ],
+            columns=['level1', 'level2', 'is_relation']
+        ).pivot_table(index='level1', columns='level2', values='is_relation').fillna(0)
+        np.fill_diagonal(matrix.values, True)  # fill diagonal to be True, true by definition -> square matrix
 
-            # check tranistivity
-            is_transitive = is_matrix_transitive(matrix)
-            # append output
-            lst_overall_set = list(OVERALL_SET)
-            lst_overall_set.sort()
-            out.append((tuple(lst_overall_set), is_transitive))
+        # check transitivity
+        is_transitive = is_matrix_transitive(matrix)
 
-            # append items to has been_checked
-            lst_has_been_checked.extend(list({*chain(*[list(item) for item in lst_overall_set])}))
+        # append output
+        lst_overall_set = list(set_cluster_candidates)
+        lst_overall_set.sort()
+        OUTPUT.append((tuple(lst_overall_set), is_transitive))
 
-    return out
+        # append items to has been_checked
+        LST_HAS_BEEN_CHECKED.extend(list({*chain(*[list(item) for item in lst_overall_set])}))
+
+        # return list of unique lemmas for each transitive cluster
+        lst_cluster_lemma = [list({*chain(*[list(i) for i in cluster[0]])}) for cluster in OUTPUT if cluster[-1]]
+
+    return lst_cluster_lemma
